da0323cad0821689af443999eced31892ca430ee
unknown <zhengbindesu@gmail.com>
2024-11-22 21:16:41 +0800

    Nanoka edit. 2024/11/22

--- inc/cube_algorithm.hpp
+++ inc/cube_algorithm.hpp
@@ -8,24 +8,26 @@
 class Algo_BFS
 {
 public:
-    Algo_BFS() : order_rc(std::make_shared<Rubik_Cube>(NANOKA_CASE_NUM, NANOKA_LAYER_NUM)), reverse_rc(std::make_shared<Rubik_Cube>(NANOKA_CASE_NUM, NANOKA_LAYER_NUM)) {}
-    
+    Algo_BFS() : order_rc(std::make_shared<Rubik_Cube>(NANOKA_CASE_NUM, NANOKA_LAYER_NUM)), reverse_rc(std::make_shared<Rubik_Cube>(NANOKA_CASE_NUM, NANOKA_LAYER_NUM)), num_pack({0, 0}) {}
+
     Algo_BFS(Algo_BFS &) = default;
     Algo_BFS(Algo_BFS &&) = default;
     ~Algo_BFS() = default;
     Algo_BFS &operator=(Algo_BFS &) = default;
     Algo_BFS &operator=(Algo_BFS &&) = default;
 
-    // 计算魔方的上帝之数 
+    // 计算魔方的上帝之数
     nanoka_num_t nanoka_compute_bfs(void);
     // 魔方开始随机初始化状态函数
-    nanoka_status_t nanoka_random_state(void);
+    nanoka_status_t nanoka_random_state(nanoka_num_t depth);
 
 private:
     // 两个魔方存储体
     std::shared_ptr<Rubik_Cube> order_rc, reverse_rc;
     // 两个魔方的状态存储
     std::map<nanoka_num_t, std::vector<nanoka_storage_t>> order_map, reverse_map;
+    // 两个魔方的层数标识
+    std::vector<nanoka_num_t> num_pack;
 };
 
 #endif
--- src/cube_algorithm.cpp
+++ src/cube_algorithm.cpp
@@ -2,8 +2,13 @@
 
 nanoka_num_t Algo_BFS::nanoka_compute_bfs(void)
 {
-    try{
-        // 我们首先旋转我们的
+    try
+    {
+        // 我们首先获取我们的初始状态
+        nanoka_random_state(5);
+
+        // 开始维护我们的状态表
+        
     }
     catch (std::runtime_error e)
     {
@@ -14,4 +19,34 @@ nanoka_num_t Algo_BFS::nanoka_compute_bfs(void)
         std::cerr << "(Algo_BFS::nanoka_compute_bfs) Unknown_error: Process crushed." << " File " << __FILE__ << ", line " << __LINE__ << "." << std::endl;
     }
     return static_cast<nanoka_num_t>(NANOKA_FAILURE);
+}
+
+nanoka_status_t Algo_BFS::nanoka_random_state(nanoka_num_t depth)
+{
+    try
+    {
+        // 对一个进行随机初始化，对另一个进行普通初始化
+        nanoka_status_t ret = reverse_rc->rubik_random_state_generator(depth);
+        if (ret == NANOKA_ERROR)
+            throw std::runtime_error("(Algo_BFS::nanoka_random_state) reverse_rc->rubik_random_state_generator failed.");
+
+        ret = order_rc->rubik_reset();
+        if (ret == NANOKA_ERROR)
+            throw std::runtime_error("(Algo_BFS::nanoka_random_state) order_rc->rubik_reset failed.");
+
+        num_pack.clear();
+        for (nanoka_num_t i; i < 2; ++i)
+            num_pack.emplace_back(0);
+
+        return NANOKA_SUCCESS;
+    }
+    catch (std::runtime_error e)
+    {
+        std::cerr << "(Algo_BFS::nanoka_random_state) Runtime_error: " << e.what() << " File " << __FILE__ << ", line " << __LINE__ << "." << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "(Algo_BFS::nanoka_random_state) Unknown_error: Process crushed." << " File " << __FILE__ << ", line " << __LINE__ << "." << std::endl;
+    }
+    return NANOKA_ERROR;
 }
